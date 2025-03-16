41ce7875532841de4c5d7ef2292168e9c4a4e297
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-15 21:07:33 -0600

    added cors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,8 @@
 class ArticlesController < ApplicationController
+
+  def index
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=#{Rails.application.credentials[:news_api_key]}")
+
+    render json: response.parse["articles"]
+  end
 end
